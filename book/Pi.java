063674a5c60b904c2bad3798b82f3f0182e2bc13
joshgilberstadt <joshgilberstadt@10.0.1.176>
2018-06-07 17:59:44 -0500

    Pi

---
+++ book/Pi.java
@@ -0,0 +1,15 @@
+
+public class Pi {
+
+	public static void main(String[] args) {
+		     
+	int a = (int) (Math.random()*2)-1;
+	int b = (int) (Math.random()*2)-1;
+	int c = 	  
+
+		     System.out.println("Our group shows Pi = " + ans);
+		  
+
+
+		  }
+		}
