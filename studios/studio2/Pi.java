c32c3aa483717d6fb26be480022a495d1869d8b4
joshgilberstadt <joshgilberstadt@10.0.1.176>
2018-06-11 14:13:35 -0500

    Ruin and Pi

--- studios/studio2/Pi.java
+++ studios/studio2/Pi.java
@@ -1,10 +1,17 @@
-package studio2;
 
 public class Pi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		     
+	int a = (int) (Math.random()*2)-1;
+	int b = (int) (Math.random()*2)-1;
+	int c = (int) Math.sqrt(Math.pow(a, 2)+ Math.pow(b, 2));
+	
+	
 
-	}
+		     System.out.println("Our group shows Pi = " + ans);
+		  
 
-}
+
+		  }
+		}
