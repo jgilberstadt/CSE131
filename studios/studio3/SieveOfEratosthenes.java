9878fd2c4662eead71025d55970901d9096e4c46
joshgilberstadt <joshgilberstadt@10.0.1.176>
2018-06-14 18:11:01 -0500

    Sieve

---
+++ studios/studio3/SieveOfEratosthenes.java
@@ -0,0 +1,27 @@
+package studio3;
+
+import cse131.ArgsProcessor;
+
+public class SieveOfEratosthenes {
+
+	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int arraySize = ap.nextInt("Size of array?");
+		if (arraySize > 2) {
+		int[] array = new int[arraySize];
+		int i;
+		int j;
+		for (i = 0; i <= (arraySize); i++) {
+		array[i] = i;
+		for (j = 2 * i; (2 * i <= j) && (j <= arraySize); j++) {   
+			for (i = 2; i <= j/2; i++){ 
+				if (array[j] % i == 0){           
+					System.out.println(j + " is not prime number");
+				}
+		}
+		}
+		}
+		}
+		}
+		}
+		
