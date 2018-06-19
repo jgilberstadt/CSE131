2f9bce45a0a56ab589f942e5832724fd5a524207
joshgilberstadt <joshgilberstadt@10.0.1.176>
2018-06-19 15:52:30 -0500

    Dice

--- labs/lab3/Dice.java
+++ labs/lab3/Dice.java
@@ -15,15 +15,19 @@ public class Dice {
 		int m = 0;
 		int n = 0;
 		int o;
+		int p = 0;
 	if ((numberOfDice > 0) && (timesThrown > 0)) {
 	for (i = 0; i < (timesThrown); i++) {
 	int[] dice = new int[numberOfDice];
 	for (j = 0; j < (numberOfDice); j++ ) {
 		dice[j] = (int) (6 * Math.random() + 1);
+	p = p + dice[j];
 	}
 		for (int temp : dice) {
 	System.out.println(temp);
 }
+	System.out.println("Sum of dice values =" + p);
+	p = 0;
 	for (k = 0; k < (numberOfDice); k++) {	
 	for (l = 0; l < (numberOfDice); l++ ) {
 	if ((dice[l] == dice[k])) {
