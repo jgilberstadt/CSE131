fc7e1ad9faee93f54f92bf40fe6e09bac92d6d54
joshgilberstadt <joshgilberstadt@10.0.1.176>
2018-06-19 15:45:17 -0500

    Dice

--- labs/lab3/Dice.java
+++ labs/lab3/Dice.java
@@ -10,6 +10,11 @@ public class Dice {
 		int timesThrown = ap.nextInt("Number of times dice are thrown?");
 		int i;
 		int j;
+		int k;
+		int l;
+		int m = 0;
+		int n = 0;
+		int o;
 	if ((numberOfDice > 0) && (timesThrown > 0)) {
 	for (i = 0; i < (timesThrown); i++) {
 	int[] dice = new int[numberOfDice];
@@ -19,7 +24,19 @@ public class Dice {
 		for (int temp : dice) {
 	System.out.println(temp);
 }
+	for (k = 0; k < (numberOfDice); k++) {	
+	for (l = 0; l < (numberOfDice); l++ ) {
+	if ((dice[l] == dice[k])) {
+	m = m + 1;
+	}
+	}
 }
+	if (m == (numberOfDice * numberOfDice)) {
+	n = n + 1;
+}
+}
+	o = 100 * n / (timesThrown);
+System.out.println("The dice all showed the same value " + o + "% of the time");
 }
 }
 }
