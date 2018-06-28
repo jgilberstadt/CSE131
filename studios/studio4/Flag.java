e06896a9fe8bc2eb8affe116e2495576ca4a1d23
joshgilberstadt <joshgilberstadt@10.0.1.176>
2018-06-28 17:06:34 -0500

    Flag

--- studios/studio4/Flag.java
+++ studios/studio4/Flag.java
@@ -1,14 +1,34 @@
 package studio4;
 
+import java.awt.Color;
+
+import java.awt.font.*;
+
 import sedgewick.StdDraw;
 
 public class Flag {
 
 	
 	public static void main(String[] args) {
-		//
-		//  Add code for your studio below here.
-		//
+	StdDraw.setPenColor(StdDraw.RED);
+	StdDraw.filledRectangle(.5, .65, .5, .1);
+	StdDraw.setPenColor(StdDraw.BLACK);
+	StdDraw.filledRectangle(.5, .45, .5, .1);
+	StdDraw.setPenColor(StdDraw.GRAY);
+	StdDraw.filledRectangle(.5, .25, .5, .1);
+	StdDraw.setPenColor(StdDraw.ORANGE);
+	double[] x = {0, 0, .3};
+	double[] y = {.75, .15, .45};
+	StdDraw.filledPolygon(x, y);
+	StdDraw.setPenColor(StdDraw.WHITE);
+	StdDraw.filledCircle(.88, .65, .05);
+	StdDraw.setPenColor(StdDraw.RED);
+	StdDraw.filledEllipse(.93, .65, .07, .05);
+	StdDraw.setPenColor(StdDraw.BLUE);
+	int i;
+	for (i = 0; i < 6; i++) {
+	StdDraw.text(.25 + (i * .1), .7 - (.1 * i), "United Gilberstadt Republic");
+	}
 	}
 
 }
