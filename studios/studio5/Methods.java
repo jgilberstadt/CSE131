ec8dac5b03f15be024b05791bdd517f1bc76995d
joshgilberstadt <joshgilberstadt@10.0.1.176>
2018-07-05 17:26:28 -0500

    Methods

--- studios/studio5/Methods.java
+++ studios/studio5/Methods.java
@@ -18,9 +18,30 @@ public class Methods {
 	 * @param y another factor
 	 * @return the product of the factors
 	 */
+	
+	/**
+	 */
 	public static int mpy(int x, int y) {
 		return 0;  // FIXME
 	}
+	
+	/**
+	 * 
+	 * @param one number
+	 * @param another number
+	 * @param yet another number
+	 * @return the average of the three numbers
+	 */
+public static double avg3(double x, double y, double z) {
+return (x + y + z) / 3;
+}
 
-
+public static double sumArray(double[] sum) {
+	double x = 3;
+	double j = 0;
+	for (double i = 0; i < x; i++) {
+		j = j + sum[(int) i];
+	}
+		return j;
+}
 }
--- studios/studio5/MethodsTest.java
+++ studios/studio5/MethodsTest.java
@@ -12,7 +12,7 @@ public class MethodsTest {
 		// Try something simple
 		//
 		assertEquals(7, Methods.sum(3, 4));
-		//
+		assertEquals(-7, Methods.sum(-3, -4));
 		// Try something fancier
 		//
 		for (int i=0; i < 10; ++i) {
@@ -22,8 +22,8 @@ public class MethodsTest {
 	
 	@Test
 	public void thisTestShouldFail() {
-		assertEquals(1, Methods.mpy(0,3));
-		assertEquals(1, Methods.mpy(2, 5));
+		assertEquals(0, Methods.mpy(0,3));
+		assertEquals(0, Methods.mpy(2, 5));
 	}
 	
 	@Test
@@ -39,7 +39,7 @@ public class MethodsTest {
 	
 	@Test
 	public void testAvg3() {
-		assertEquals(2.0, Methods.avg3(1,2,3), 0.1);		
+		assertEquals(2.0, Methods.avg3(1.0,2.0,3.0), 0.1);		
 	}
 	
 	@Test
