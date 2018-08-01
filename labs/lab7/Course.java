b09dbbba119d5395aabb76b326fe62bc7d237f63
joshgilberstadt <joshgilberstadt@10.0.1.176>
2018-07-31 21:02:15 -0500

    Course and student

---
+++ labs/lab7/Course.java
@@ -0,0 +1,102 @@
+package lab7;
+
+public class Course {
+
+	private String Name;
+	private int Credits, NumSeats;
+	private Student[] Roster;
+	
+	public Course(String a, int b, int c) {
+	this.Roster = new Student[c];
+	this.Credits = b;
+	this.NumSeats = c;
+	this.Name = a;
+}
+	
+	public boolean addStudent(Student S) {
+	int j = 0;
+	for (int i = 0; i < Roster.length; i++) {
+	if (this.Roster[i] == S) {
+	j = j + 1;	
+	}
+	}
+	boolean c = ((this.NumSeats > 0)&&(j == 0));	
+	if (c == true) {
+	for (int i = 0; i < Roster.length; i++) {
+	if (this.Roster[i] == null) {
+	this.Roster[i] = S;
+	break;
+	}
+	}
+	this.NumSeats = this.NumSeats - 1;
+	}
+	return c;
+	}
+	
+	public String generateRoster() {
+	String S= "";
+	for (int i = 0; i < Roster.length; i++)	{
+	S = S + this.Roster[i] + " ";	
+	}
+	return S;
+	}
+	
+	public int getRemainingSeats() {
+	return NumSeats;
+	}
+	
+	public double averageGPA() {
+	int j = 0;
+	double h = 0.0;
+	for (int i = 0; i < Roster.length; i++)	{
+	if (this.Roster[i] != null) {
+	h = h + this.Roster[i].getGPA();
+	j = j + 1;
+	}
+	}
+	h = h / (double) j;
+	return h;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public int getCredits() {
+		return Credits;
+	}
+
+	public void setCredits(int credits) {
+		Credits = credits;
+	}
+
+	public int getNumSeats() {
+		return NumSeats;
+	}
+
+	public void setNumSeats(int numSeats) {
+		NumSeats = numSeats;
+	}
+
+	public Student[] getRoster() {
+		return Roster;
+	}
+	
+	public void setRoster(Student[] roster) {
+		Roster = roster;
+	}
+
+	public String toString() {
+	return this.getName() + this.getCredits() + this.getNumSeats() + this.getRoster();	
+	}
+	
+	public static void main(String[] args) {
+	Course c = new Course(Algebra, 3, 3, 10, Andrew, Beth, Carl, Dana, Evan, Frank, Gilbert, Hallie, Isaac, Me);
+
+	}
+
+}
---
+++ labs/lab7/Student.java
@@ -0,0 +1,82 @@
+package lab7;
+
+public class Student {
+
+	private double GPA;
+	private int CreditsTotal, StudentID;
+	private String First, Last, ClassStanding;
+	
+	
+	public Student (String a, String b, int c) {
+	this.CreditsTotal = 0;
+	this.GPA = 0.0;
+	this.StudentID = c;
+	this.First = a;
+	this.Last = b;
+	this.ClassStanding = "Freshman";
+	}
+	
+	private Student (String a, String b, int c, double d, int e) {
+		this.CreditsTotal = e;
+		this.GPA = d;
+		this.StudentID = c;
+		this.First = a;
+		this.Last = b;
+		
+	}
+	
+	public void submitGrade(double b, int d) {
+	this.GPA = Math.round((this.GPA * this.CreditsTotal + d * b) / (this.CreditsTotal+ d) * 1000) / 1000.0;
+	this.CreditsTotal = this.CreditsTotal + d;
+	}
+	
+	public String getClassStanding() {
+	if (this.CreditsTotal < 30) {
+	return "Freshman";
+	}else if ((this.CreditsTotal >= 30)&&(this.CreditsTotal < 60)) {
+	return "Sophomore";
+	}else if ((this.CreditsTotal >= 60)&&(this.CreditsTotal < 90)) {
+	return "Junior";
+	}else {
+	return "Senior";
+	}
+	}
+	
+	public Student createLegacy(Student T) {
+	Student A = new Student(this.getName(), T.getName(), this.StudentID + T.getStudentID(), this.GPA, this.CreditsTotal);
+	A.GPA = (this.GPA + T.getGPA()) / 2.0;
+	A.CreditsTotal = Math.max(this.CreditsTotal, T.getCredits());
+	return A;
+	}
+	
+	
+	
+	
+	public int getStudentID() {
+		return this.StudentID;
+	}
+
+	public String getName() {
+		return this.First + " " + this.Last;
+	}
+	
+	public int getCredits() {
+		return this.CreditsTotal;
+	}
+	
+	public String toString() {
+	return this.getName() + this.getStudentID();
+	}
+	
+	public double getGPA() {
+		return this.GPA;
+	}
+
+
+//	public static void main(String[] args) {
+//	Student s = new Student (38, 3.54, 3.7, 3, "Sam", "Smith", "Jenny", "Johnson", 126, 122, 471835, 274824, 3.46, 3.58, null, null);
+//	System.out.println("Student Profile? " + s);
+//	}
+	}
+
+
