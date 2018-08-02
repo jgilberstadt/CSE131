298c5b3c57f734cf98cffa42e73d504853c203df
joshgilberstadt <joshgilberstadt@10.0.1.176>
2018-08-02 18:46:14 -0500

    Date and Time

--- studios/studio8/Date.java
+++ studios/studio8/Date.java
@@ -1,9 +1,40 @@
 package studio8;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import exercises8.Valuable;
+
 public class Date {
 	
+	private final List<Date> Month, Day, Year;
+	private final String Holiday;
+	
+	public Date() {
+		this.Month = new LinkedList<Date>();
+		this.Day = new LinkedList<Date>();
+		this.Year = new LinkedList<Date>();
+	}
+	
+	public void addMonth(Date m) {
+		Month.add(m);
+	}
+	
+	public void addDay(Date d) {
+		Month.add(d);
+	}
+	
+	public void addYear(Date y) {
+		Month.add(y);
+	}
+	
+	public String toString() {
+	return 	
+	}
 	
 	public static void main(String[] args) {
+	Date d = new Date();
+	System.out.println("Date: " + d);
 	}
 
 }
--- studios/studio8/Time.java
+++ studios/studio8/Time.java
@@ -1,9 +1,48 @@
 package studio8;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Time {
 	
+	private final List<Time> Hour, Minute;
+	private final int Format;
+	
+	public Time(int a) {
+		this.Hour = new LinkedList<Time>();
+		this.Minute = new LinkedList<Time>();
+		this.Format = a;
+	}
+	
+	public void addHour(Time h) {
+		Hour.add(h);
+	}
+	
+	public int Hour(int b) {
+		if ((this.Format == 12)&&(b > 12)) {
+		return b - 12;
+		}else {
+			return b;
+		}
+	}
+	
+	public void addMinute(Time m) {
+		Minute.add(m);
+	}
+	
+	public int Minute(int c) {
+		return c;
+	}
+	
+	public String toString() {
+		return Hour(4) + ": " + Minute(40);
+	}
 	
 	public static void main(String[] args) {
+	Time t = new Time(12);
+	t.Hour (4);
+	t.Minute(30);
+	System.out.println("Time: " + t);
 	}
 
 }
